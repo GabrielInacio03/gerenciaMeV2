27018692ed5f9bd9645ccec1e6d525eb4aa76bae
GabrielInacio03 <inaciogs03@gmail.com>
2022-12-03 16:35:06 -0300

    edições

--- resources/views/layouts/appDashboard.blade.php
+++ resources/views/layouts/appDashboard.blade.php
@@ -25,11 +25,14 @@
     <!-- Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
     <!-- CSS3 -->
-    <link rel="stylesheet" href="{{ URL::asset('css/default.css') }}">
 
     <!-- Chartjs -->
 
     <style>
+       button.navbar-toggler .position-relative .d-md-none .collapsed{
+        color: #FFF;
+        background-color: #FFF;
+       }
         #principal{
             margin: 0 auto;
         }
@@ -67,7 +70,7 @@
     <nav class="navbar navbar-expand-lg  bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Gerencia<sup>Me</sup></a>
-            <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler position-relative d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
@@ -132,7 +135,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
-    <script src="{{ URL::asset('js/dashboard.js') }}"></script>
 </body>
 
 
